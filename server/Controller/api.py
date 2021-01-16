a005e822c6e3c90722a526cb605b85e295440878
yaraxxx <pyara-100@hotmail.com>
2021-01-16 18:24:43 +0300

    Testing the setup code : Passed

--- server/Controller/api.py
+++ server/Controller/api.py
@@ -3,12 +3,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import time
 
-
-# app = Flask(__name__, instance_relative_config=True)
-# app.config.from_object('config')
-# app.config.from_pyfile('config.py')
-
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
 
 @app.route('/time')
@@ -17,4 +13,4 @@ def get_current_time():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
--- server/Controller/config.py
+++ server/Controller/config.py
@@ -3,5 +3,5 @@ SECRET_KEY = os.urandom(32)
 # Enable debug mode.
 DEBUG = True
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgres://yarasaleh@localhost:5432/tera'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+# SQLALCHEMY_DATABASE_URI = 'postgres://yarasaleh@localhost:5432/tera'
+# SQLALCHEMY_TRACK_MODIFICATIONS = False
